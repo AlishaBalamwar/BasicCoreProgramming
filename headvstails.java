1c6df89f6673a1d85866c16e2a77a6be4d401f06
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-08-23 16:17:17 +0530

    welcome to basic programming

---
+++ headvstails.java
@@ -0,0 +1,6 @@
+package headsvstails;
+public class headvstails{
+	public static void main(String[] args){  
+	     System.out.println(" Welcome to Basic core Programming Problems");  
+	    }  
+	}  
