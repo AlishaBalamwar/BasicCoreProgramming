6fffba55c5d6c7b1b765f285417f08accaabaf8f
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-08-23 16:45:58 +0530

    it checks whether the year is leap or not

--- headvstails.java
+++ headvstails.java
@@ -2,29 +2,20 @@ package headsvstails;
 import java.util.Scanner;
 public class headvstails {
  public static void main(String[] args) {
-  Scanner sc = new Scanner(System.in);	 
-  System.out.println("Enter the number of flips");
-  int n=sc.nextInt();
-  double var=0;
-  for (int i=0; i<n; i++){
-	  double result=Math.floor(Math.random()*10)%2;
-	  if(result==0) {
-		  System.out.println("It is tail");
-		  var+=1;
-	  }
-	  else {
-		  System.out.println("It is head");
-	  }
-	}
- System.out.println("Percentage of tail is "+var/n);
- System.out.println("Percentage of head  is "+(n-var)/n);
- double tail = var/n;
- double head=(n-var)/n;
- if(tail<=0.5)
- {System.out.println("It is tail because percentage of tail is less than or equal to 0.5");
- }
- else {
-	 System.out.println("It is head because percentage of head is more than 0.5");
- }
+	 int year;
+     System.out.println("Enter a Year : ");
+     Scanner sc = new Scanner(System.in);
+     year = sc.nextInt();
+if((year>999) && (year<10000)) {
+     if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0)) {
+        System.out.println(year+" year is a leap year");
+     }
+     else {
+        System.out.println(year+" year is not a leap year");
+     }
 }
+else
+{System.out.println("Invalid input");
 }
+}  
+}  
