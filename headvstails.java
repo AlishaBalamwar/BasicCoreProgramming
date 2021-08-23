27c4770de053f6ebfc4dd60ce9fd736f51fa21c0
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-08-23 18:19:48 +0530

    It checks largest between three numbers

--- headvstails.java
+++ headvstails.java
@@ -2,14 +2,22 @@ package headsvstails;
 import java.util.Scanner;
 public class headvstails {
 	public static void main(String[] args) {
-		System.out.print("Enter a character : ");
-        Scanner sc = new Scanner(System.in);
-        char ch = sc.next().charAt(0);
-        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I'
-                || ch == 'O' || ch == 'U') {
-            System.out.println(ch + " character is an vowel");
-        } else {
-            System.out.println(ch + " character is a consonant");
+		Scanner s = new Scanner(System.in);
+        System.out.println("Find the Largest Number among Three number");
+        System.out.print("Enter 1st num: ");
+        int a = s.nextInt();
+        System.out.print("Enter 2nd num: ");
+        int b = s.nextInt();
+        System.out.print("Enter 3rd num: ");
+        int c = s.nextInt();
+        if(a>b && a>c)
+        {
+            System.out.println(a+" is Largest");
+        }else if(b>a && b>c)
+        {
+            System.out.println(b+" is Largest");
+        }else {
+            System.out.println(c+" is Largest");
         }
     }
 }
