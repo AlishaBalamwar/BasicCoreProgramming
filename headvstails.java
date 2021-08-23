d10f6c0b05a5b509de377cae49a4634cbea312ee
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-08-23 17:06:55 +0530

    It prints the power table of 2

--- headvstails.java
+++ headvstails.java
@@ -1,21 +1,14 @@
 package headsvstails;
 import java.util.Scanner;
 public class headvstails {
- public static void main(String[] args) {
-	 int year;
-     System.out.println("Enter a Year : ");
-     Scanner sc = new Scanner(System.in);
-     year = sc.nextInt();
-if((year>999) && (year<10000)) {
-     if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0)) {
-        System.out.println(year+" year is a leap year");
+ int var = 1,i;
+ System.out.println("Enter value of power till which we have to print table of 2: ");
+ Scanner sc = new Scanner(System.in);
+ Long z=sc.nextLong();
+
+     for ( i = 1;i<=z; i++) {
+         var = var * 2;
+         System.out.println("2 ^ " + i + " = " + var);
      }
-     else {
-        System.out.println(year+" year is not a leap year");
-     }
+ }
 }
-else
-{System.out.println("Invalid input");
-}
-}  
-}  
