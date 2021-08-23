9ca7e161e23ec43b60f933db2252854c7faf7bfd
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-08-23 17:26:13 +0530

    it shows harmonic series sum

--- headvstails.java
+++ headvstails.java
@@ -1,16 +1,18 @@
 package headsvstails;
 import java.util.Scanner;
 public class headvstails {
-	 public static void main(String[] args) {
- int var = 1,i;
- System.out.println("Enter value of power till which we have to print table of 2: ");
- Scanner sc = new Scanner(System.in);
- Long z=sc.nextLong();
+		 public static void main(String[] args) {
+		        float harmonicSeriesSum = 1;
+		        Scanner sc=new Scanner(System.in);
+		        System.out.print("Enter number for print Harmonic series: ");
+		int n= sc.nextInt();
+		for(int i=1;i<=n;i++){
+		    
+		    System.out.print("1/"+i+" + ");
+		    harmonicSeriesSum += (float)1 / i;
+		}
+		
+		System.out.println("Sum of Harmonic series is: "+harmonicSeriesSum);
 
-     for ( i = 1;i<=z; i++) {
-         var = var * 2;
-         System.out.println("2 ^ " + i + " = " + var);
-     }
-	 }
- 
-}
+		    }
+		}
