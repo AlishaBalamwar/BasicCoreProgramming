dad6296fb96eb8c934d3c5eb2f83dff8f4d1b6fb
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-08-23 17:18:26 +0530

    previously wrong file was added in power of 2 table where main method was not there

--- headvstails.java
+++ headvstails.java
@@ -1,6 +1,7 @@
 package headsvstails;
 import java.util.Scanner;
 public class headvstails {
+	 public static void main(String[] args) {
  int var = 1,i;
  System.out.println("Enter value of power till which we have to print table of 2: ");
  Scanner sc = new Scanner(System.in);
@@ -10,5 +11,6 @@ public class headvstails {
          var = var * 2;
          System.out.println("2 ^ " + i + " = " + var);
      }
- }
+	 }
+ 
 }
